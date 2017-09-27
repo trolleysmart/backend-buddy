9c235c4f8c9d5c4168c07e0c213c106954e75cef
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2017-09-28 06:53:58 +1300

    App ID changed

--- cloud/main.js
+++ cloud/main.js
@@ -5,7 +5,7 @@ var GraphQLHTTP = require('express-graphql');
 var Parse = require('parse/node');
 var getRootSchema = require('trolley-smart-backend-graphql').getRootSchema;
 
-Parse.initialize('7fd8e761-e3fd-4348-b876-ea6d071fbe14', 'ox9WktuYlln61YrunDXUBBjc0zbFFoJv'|| 'unused', 'ZBjVnnEBfY39sf57p7M77DSyiIBWwctw');
+Parse.initialize('50a47f7f-411a-4abb-8c50-3daabac420eb', 'w2GaCmTc2U7QwjbR3NGA1cg0UTjvbSYE' || 'unused', 'p35twdsjRGwe7vo2S1L654i2r5dcAT0C');
 Parse.serverURL = 'https://parse.buddy.com/parse';
 
 const expressServer = Express();
