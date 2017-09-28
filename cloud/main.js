3eb34dbd6dbe6a4ef635cac0f9a50a25b794109c
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2017-09-28 20:14:25 +1300

    Code refactored

--- cloud/main.js
+++ cloud/main.js
@@ -5,7 +5,11 @@ var GraphQLHTTP = require('express-graphql');
 var Parse = require('parse/node');
 var getRootSchema = require('trolley-smart-backend-graphql').getRootSchema;
 
-Parse.initialize('50a47f7f-411a-4abb-8c50-3daabac420eb', 'w2GaCmTc2U7QwjbR3NGA1cg0UTjvbSYE' || 'unused', 'p35twdsjRGwe7vo2S1L654i2r5dcAT0C');
+const applicationId = '50a47f7f-411a-4abb-8c50-3daabac420eb';
+const javascriptKey = 'w2GaCmTc2U7QwjbR3NGA1cg0UTjvbSYE';
+const masterKey = 'p35twdsjRGwe7vo2S1L654i2r5dcAT0C';
+
+Parse.initialize(applicationId, javascriptKey|| 'unused', masterKey);
 Parse.serverURL = 'https://parse.buddy.com/parse';
 
 const expressServer = Express();
