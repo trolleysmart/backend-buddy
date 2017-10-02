cc765bd00dcf081480de93f07f908fa042c6bab3
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2017-10-03 00:09:14 +1300

    Bug fixed

--- cloud/main.js
+++ cloud/main.js
@@ -1,117 +1,52 @@
-'use strict';
-
 require('regenerator-runtime/runtime');
 
-var _immutable = require('immutable');
+const { Map } = require('immutable');
+const Express = require('express');
+const GraphQLHTTP = require('express-graphql');
+const Parse = require('parse/node');
+const { getRootSchema } = require('trolley-smart-backend-graphql');
+const { ParseWrapperService, UserService } = require('micro-business-parse-server-common');
+const { ShoppingListService, StapleTemplateItemService } = require('trolley-smart-parse-server-common');
 
-var _express = require('express');
+const applicationId = '50a47f7f-411a-4abb-8c50-3daabac420eb';
+const javascriptKey = 'w2GaCmTc2U7QwjbR3NGA1cg0UTjvbSYE';
+const masterKey = 'p35twdsjRGwe7vo2S1L654i2r5dcAT0C';
 
-var _express2 = _interopRequireDefault(_express);
+Parse.initialize(applicationId, javascriptKey || 'unused', masterKey);
+Parse.serverURL = 'https://parse.buddy.com/parse';
 
-var _expressGraphql = require('express-graphql');
+const expressServer = Express();
+const schema = getRootSchema();
 
-var _expressGraphql2 = _interopRequireDefault(_expressGraphql);
+expressServer.use('/graphql', GraphQLHTTP({ schema, graphiql: true }));
 
-var _node = require('parse/node');
+expressServer.use('/afterSaveUser', (req, res) => {
+  let body = '';
 
-var _node2 = _interopRequireDefault(_node);
-
-var _microBusinessParseServerCommon = require('micro-business-parse-server-common');
-
-var _trolleySmartBackendGraphql = require('trolley-smart-backend-graphql');
-
-var _trolleySmartParseServerCommon = require('trolley-smart-parse-server-common');
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
-
-var applicationId = '50a47f7f-411a-4abb-8c50-3daabac420eb';
-var javascriptKey = 'w2GaCmTc2U7QwjbR3NGA1cg0UTjvbSYE';
-var masterKey = 'p35twdsjRGwe7vo2S1L654i2r5dcAT0C';
-
-_node2.default.initialize(applicationId, javascriptKey || 'unused', masterKey);
-_node2.default.serverURL = 'https://parse.buddy.com/parse';
-
-var expressServer = (0, _express2.default)();
-var schema = (0, _trolleySmartBackendGraphql.getRootSchema)();
-
-expressServer.use('/graphql', (0, _expressGraphql2.default)({
-  schema: schema,
-  graphiql: true
-}));
-
-var addDefaultShoppingList = function () {
-  var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(user) {
-    return regeneratorRuntime.wrap(function _callee$(_context) {
-      while (1) {
-        switch (_context.prev = _context.next) {
-          case 0:
-            return _context.abrupt('return', new _trolleySmartParseServerCommon.ShoppingListService().create((0, _immutable.Map)({ name: 'My List', user: user, status: 'A' }), _microBusinessParseServerCommon.ParseWrapperService.createACL(user)));
-
-          case 1:
-          case 'end':
-            return _context.stop();
-        }
-      }
-    }, _callee, undefined);
-  }));
-
-  return function addDefaultShoppingList(_x) {
-    return _ref.apply(this, arguments);
-  };
-}();
-
-var onAfterSaveUser = function () {
-  var _ref2 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee2(request) {
-    var user;
-    return regeneratorRuntime.wrap(function _callee2$(_context2) {
-      while (1) {
-        switch (_context2.prev = _context2.next) {
-          case 0:
-            if (!(request.object.createdAt !== request.object.updatedAt)) {
-              _context2.next = 2;
-              break;
-            }
-
-            return _context2.abrupt('return');
-
-          case 2:
-            _context2.next = 4;
-            return _microBusinessParseServerCommon.UserService.getUserById(request.object.objectId);
-
-          case 4:
-            user = _context2.sent;
-            _context2.next = 7;
-            return Promise.all([new _trolleySmartParseServerCommon.StapleTemplateItemService().cloneStapleTemplateItems(user), addDefaultShoppingList(user)]);
-
-          case 7:
-          case 'end':
-            return _context2.stop();
-        }
-      }
-    }, _callee2, undefined);
-  }));
-
-  return function onAfterSaveUser(_x2) {
-    return _ref2.apply(this, arguments);
-  };
-}();
-
-expressServer.use('/afterSaveUser', function (req, res) {
-  var body = '';
-
-  req.on('data', function (chunk) {
+  req.on('data', (chunk) => {
     body += chunk;
   });
 
-  req.on('end', function () {
-    onAfterSaveUser(JSON.parse(body)).then(function () {
+  req.on('end', () => {
+    const request = JSON.parse(body);
+
+    if (request.object.createdAt !== request.object.updatedAt) {
       res.writeHead(200);
       res.end();
-    }).catch(function (error) {
-      throw new Error(error);
-    });
+
+      return;
+    }
+
+    UserService.getUserById(request.object.objectId)
+      .then(user => Promise.all(
+        [new StapleTemplateItemService().cloneStapleTemplateItems(user),
+          new ShoppingListService().create(Map({ name: 'My List', user, status: 'A' }), ParseWrapperService.createACL(user))]))
+      .then(() => {
+        res.writeHead(200);
+        res.end();
+      }).catch((error) => {
+        throw new Error(error);
+      });
   });
 });
 
